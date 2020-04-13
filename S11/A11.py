0bf02bc6eb69f35fb36a09acdee4f9d43e8fb0c4
bhargav23 <bhargav.kiran@gmail.com>
2020-04-13 14:24:26 +0530

    Update A11.py

--- S11/A11.py
+++ S11/A11.py
@@ -4,8 +4,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-dropout_value = 0.07
-
 class newResnet(nn.Module):
     def __init__(self):
         super(newResnet, self).__init__()
@@ -13,16 +11,14 @@ class newResnet(nn.Module):
         self.PrepLayer = nn.Sequential(
           nn.Conv2d(3, 64, 3,padding=1),
           nn.ReLU(),
-          nn.BatchNorm2d(64),
-          nn.Dropout(dropout_value)      
+          nn.BatchNorm2d(64),      
         )
 
         self.layer1convblock1 = nn.Sequential(
           nn.Conv2d(64, 128, 3,padding=1),
           nn.MaxPool2d(2, 2),
           nn.ReLU(),
-          nn.BatchNorm2d(128),
-          nn.Dropout(dropout_value)      
+          nn.BatchNorm2d(128),   
         )
 
         self.resnet1 = nn.Sequential(
@@ -32,23 +28,20 @@ class newResnet(nn.Module):
           nn.Conv2d(128, 128, 3,padding=1),
           nn.BatchNorm2d(128),
           nn.ReLU(),
-          nn.Dropout(dropout_value) 
         )
 
         self.layer2convblock2 = nn.Sequential(
             nn.Conv2d(128, 256, 3,padding=1),
             nn.MaxPool2d(2, 2),
             nn.ReLU(),
-            nn.BatchNorm2d(256),
-            nn.Dropout(dropout_value)      
+            nn.BatchNorm2d(256),    
         )
        
         self.layer3convblock3 = nn.Sequential(
           nn.Conv2d(256, 512, 3,padding=1),
           nn.MaxPool2d(2, 2),
           nn.ReLU(),
-          nn.BatchNorm2d(512),
-          nn.Dropout(dropout_value)      
+          nn.BatchNorm2d(512),    
         )
 
         self.resnet3 = nn.Sequential(
@@ -58,7 +51,6 @@ class newResnet(nn.Module):
           nn.Conv2d(512, 512, 3,padding=1),
           nn.BatchNorm2d(512),
           nn.ReLU(),
-          nn.Dropout(dropout_value) 
         )
         
         self.pool1 = nn.MaxPool2d(4, 1)
